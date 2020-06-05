74e6f9063bdf98b9f1232374e64c7daaa8c965f4
mouadlafnoune <mouad.lafnoune1@gmail.com>
2020-06-05 15:12:09 +0100

    first commit

---
+++ src/main/java/com/mouad2code/GestionStockProduits/GestionStockProduitsApplication.java
@@ -0,0 +1,13 @@
+package com.mouad2code.GestionStockProduits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionStockProduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionStockProduitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mouad2code/GestionStockProduits/GestionStockProduitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mouad2code.GestionStockProduits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionStockProduitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
